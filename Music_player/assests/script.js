19ce5d3e3795fc029960604bb39ff6e8cad740ea
VivekRanjan620 <143477763+VivekRanjan620@users.noreply.github.com>
2024-03-18 22:34:39 +0530

    first

---
+++ Music_player/assests/script.js
@@ -0,0 +1,33 @@
+let progress = document.getElementById("progress");
+let song = document.getElementById("song");
+let playpause = document.getElementById("play-pause");
+let actionIcon = document.getElementById("action");
+
+song.addEventListener("loadedmetadata", () => {
+  progress.max = song.duration;
+  progress.value = song.currentTime;
+});
+
+playpause.addEventListener("click", () => {
+  if (actionIcon.classList.contains("fa-play")) {
+    song.play();
+    actionIcon.classList.remove("fa-play");
+    actionIcon.classList.add("fa-pause");
+  } else {
+    song.pause();
+    actionIcon.classList.remove("fa-pause");
+    actionIcon.classList.add("fa-play");
+  }
+});
+song.onplaying = function () {
+  setInterval(() => {
+    progress.value = song.currentTime;
+  }, 500);
+};
+
+progress.addEventListener("change",()=>{
+    song.play();
+    song.currentTime = progress.value;
+    actionIcon.classList.remove("fa-play");
+    actionIcon.classList.add("fa-pause");
+});
